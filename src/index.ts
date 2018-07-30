a3eddd835ffe86e1e3dc32e436a54e0a267208b3
Chris Buttery <info@chrisbuttery.com>
2018-07-30 13:38:09 +1000

    fix typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 const pattern = /(^0(2|8|9){1}[0-9]{2})|(^[1-9]{1}[0-9]{3})$/
 
-export default (postcode: string) => pattern.test(postcode)
+const isAusPostcode = (postcode: string) => pattern.test(postcode)
+
+export default isAusPostcode
